0cf8cfeb911f2709c49d58e27501a05f2e351057
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-02-23 12:01:55 +0300

    Updae Doc and Add Login Function and Add Create , Display Medical

--- pharmaceutical/Pharmaceutical.cs
+++ pharmaceutical/Pharmaceutical.cs
@@ -2,8 +2,29 @@
 
 public class Pharmaceutical
 {
-internal string Name {get;set;}
-internal int Amout {get;set;}
-internal decimal Price{get;set;}
-internal string DocName{get;set;}
+    public string Name { get; set; } = string.Empty;
+    public int Amout { get; set; }
+    public decimal Price { get; set; }
+    public string DocName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public Pharmaceutical() { }
+    public Pharmaceutical(string name, int amout, decimal price, string docName)
+    {
+        Name = name;
+        Amout = amout;
+        Price = price;
+        DocName = docName;
+        CreatedAt = DateTime.Now;
+    }
+    internal void Display()
+    {
+        Console.WriteLine($@"Name: {Name},
+     Amout: {Amout},
+      Price: {Price},
+       DocName: {DocName},
+        CreatedAt: {CreatedAt}");
+        System.Console.WriteLine("=============================\n");
+    }
+
 }
---
+++ pharmaceutical/PharmaceuticalManagment.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+
+namespace Medical;
+
+public class PharmaceuticalManagment
+{
+    public PharmaceuticalManagment()
+    {
+        load();
+    }
+    private List<Pharmaceutical> pharmaceuticals = new();
+    private readonly string filePath = "pharmaceuticals.json";
+
+    #region Create Medicin
+
+    internal List<Pharmaceutical> CreateMedicin(Pharmaceutical pharmaceutical)
+    {
+        if (pharmaceuticals.Any(p => !string.IsNullOrWhiteSpace(p.Name) && p.Name.Equals(pharmaceutical.Name, StringComparison.OrdinalIgnoreCase)))
+        {
+            Console.WriteLine("Medicine with this name already exists.");
+            return pharmaceuticals;
+        }
+        pharmaceuticals.Add(pharmaceutical);
+        Console.WriteLine("Medicine added successfully.");
+        Save();
+        return pharmaceuticals;
+    }
+
+    #endregion
+
+    #region DisplayAll
+    internal List<Pharmaceutical> DisplayAll()
+    {
+        if (pharmaceuticals.Count == 0)
+        {
+            Console.WriteLine("No medicines available.");
+            return new List<Pharmaceutical>();
+        }
+
+        return pharmaceuticals;
+    }
+    #endregion
+
+
+    #region Save
+
+    private void Save()
+    {
+
+        var op = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        string json = JsonSerializer.Serialize(pharmaceuticals, op);
+        File.WriteAllText(filePath, json);
+    }
+
+    #endregion
+
+    #region Load
+
+    internal void load()
+    {
+        if (!File.Exists(filePath))
+
+            return;
+        string json = File.ReadAllText(filePath);
+        pharmaceuticals = JsonSerializer.Deserialize<List<Pharmaceutical>>(json) ?? new List<Pharmaceutical>();
+
+
+    }
+
+
+    #endregion
+
+}
+
+
+
